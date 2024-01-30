5bce9805429c2f882d74cae8298994bd637f703d
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-01-30 11:39:15 +0100

    base

--- src/public/serviceworker.js
+++ src/public/serviceworker.js
@@ -5,3 +5,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     console.log('Service worker activated.');
 });
+
+self.addEventListener('message', (event) => {
+    console.log("Message received:", event.data);
+})
